cb0e5025ebfa48e327fe7f7f8bb229047bb8674d
Diego <diego.bano@ug.uchile.cl>
2017-09-30 18:19:57 -0400

    Added insert function, missing split

--- T1.cpp
+++ T1.cpp
@@ -1,22 +1,33 @@
 // Example program
 #include <iostream>
+#include <stdlib.h>
 #include <list>
 #include <string>
+#include <limits>
 using namespace std;
 
-#define M 2
+#define M 256
+#define MAX_INT numeric_limits<int>::max()
 
 struct Rectangle {
   int xMin, yMin;
   int xMax, yMax;
 };
 
-struct Node {
+struct Rtree {
   int isLeaf;
   int numKeys;
-  Rectangle MBR[M];
-  Node *children[M];
+  Rectangle MBR[M + 1];
+  Rtree *children[M + 1];
 };
+
+int getArea(Rectangle c) {
+	return (c.xMax - c.xMin) * (c.yMax - c.yMin);
+}
+
+int getPerimeter(Rectangle c) {
+	return 2 * (c.xMax - c.xMin) + 2 * (c.yMax - c.yMin);
+}
 
 int intersect(Rectangle r1, Rectangle r2) {
   if (r1.xMax < r2.xMin || r2.xMax < r1.xMin || r1.yMax < r2.yMin ||
@@ -26,7 +37,7 @@ int intersect(Rectangle r1, Rectangle r2) {
     return true;
 }
 
-void searchRec(Rectangle C, Node *node, list<Rectangle> &result) {
+void searchRec(Rectangle C, Rtree *node, list<Rectangle> &result) {
   if (node == NULL) {
     return;
   }
@@ -41,11 +52,66 @@ void searchRec(Rectangle C, Node *node, list<Rectangle> &result) {
   }
 }
 
-list<Rectangle> search(Rectangle C, Node *root) {
+list<Rectangle> search(Rectangle C, Rtree *root) {
   list<Rectangle> result;
   searchRec(C, root, result);
   cout << &result << "\n";
   return result;
+}
+
+int getMBRgrowth(Rectangle c, Rectangle mbr) {
+	Rectangle n;
+	n.xMin = c.xMin < mbr.xMin ? c.xMin : mbr.xMin;
+	n.xMax = c.xMax > mbr.xMax ? c.xMax : mbr.xMax;
+	n.yMin = c.yMin < mbr.yMin ? c.yMin : mbr.yMin;
+	n.yMax = c.yMax > mbr.yMax ? c.yMax : mbr.yMax;
+	a1 = getArea(n);
+	a2 = getArea(mbr);
+	return a1 - a2;
+}
+
+void fit(Rectangle c, Rectangle &mbr) {
+	mbr.xMin = c.xMin < mbr.xMin ? c.xMin : mbr.xMin;
+	mbr.xMax = c.xMax > mbr.xMax ? c.xMax : mbr.xMax;
+	mbr.yMin = c.yMin < mbr.yMin ? c.yMin : mbr.yMin;
+	mbr.yMax = c.yMax > mbr.yMax ? c.yMax : mbr.yMax;
+}
+
+void split(Rtree* node, Rtree* father) {
+	// Implementar heuristica
+}
+
+void insert(Rectangle c, Rtree *node, Rtree* father) {
+	int smaller = 0;
+	int growth = MAX_INT;
+	int tmp, a1, a2;
+	if (!node->isLeaf) {
+		for(int i=0; i < node->numKeys; i++) {
+			tmp = getMBRgrowth(c, node->MBR[i]);
+			if (tmp < growth) {
+				smaller = i;
+				growth = tmp;
+			} else if (tmp == growth) {
+				a1 = getArea(node->MBR[smaller]);
+				a2 = getArea(node->MBR[i]);
+				if (a1 > a2) {
+					smaller = i;
+					growth = tmp;
+				} else if (a1 == a2) {
+					int r = rand() % 2;
+					smaller = r ? i : smaller;
+					growth = r ? tmp : growth;
+				}
+			}
+		}
+		fit(c, node->MBR[smaller]);
+		insert(c, node->children[smaller], &node);
+	} else {
+		node->MBR[numKeys++] = c;
+	}
+	if (node->numKeys > M) {
+		split(node, father);
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -60,7 +126,7 @@ int main(int argc, char *argv[]) {
   Rectangle r8 = {-4, 3, -1, 4};
   Rectangle r9 = {3, -6, 4, -4};
 
-  Node *nodes = (Node *)malloc(5 * sizeof(Node));
+  Rtree *nodes = (Rtree *)malloc(5 * sizeof(Rtree));
   nodes[4].isLeaf = true;
   nodes[4].numKeys = 1;
   nodes[4].MBR[0] = r9;
@@ -82,7 +148,7 @@ int main(int argc, char *argv[]) {
   nodes[0].children[0] = &nodes[2];
   nodes[0].children[1] = &nodes[3];
 
-  Node *root = (Node *)malloc(sizeof(Node));
+  Rtree *root = (Rtree *)malloc(sizeof(Rtree));
   root->isLeaf = false;
   root->numKeys = 2;
   root->MBR[0] = r1;
