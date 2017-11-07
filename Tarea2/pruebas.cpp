3342f678122e1b30f643427134218df5a77a6ea1
Diego Bano <diego.bano@ug.uchile.cl>
2017-11-06 19:16:21 -0500

    Added object building in pruebas.cpp

--- Tarea2/pruebas.cpp
+++ Tarea2/pruebas.cpp
@@ -21,14 +21,18 @@ int main(int argc, char const *argv[])
 	unsigned int i = 0;
 	chrono::high_resolution_clock::time_point begin, end;
     chrono::duration<double> elapsed;
-    Hashing *h;
+    Hashing **h;
+    Ternario **t;
+    Ptrie **p;
 
-    h = new Hashing[ejemplos];
+    h = new Hashing*[ejemplos];
+    t = new Ternario*[ejemplos];
+    p = new Ptrie*[ejemplos];
 
     for (int k = 0; k < ejemplos; k++) {
-		text.open("ejemplo"+to_string(i+1)+".txt");
-		formatted.open("formateado"+to_string(i+1)+".txt", ios::in | ios::out | ios::trunc);
-		cout << "Formateando texto " << i << "\n";
+		text.open("ejemplo"+to_string(k+1)+".txt");
+		formatted.open("formateado"+to_string(k+1)+".txt", ios::in | ios::out | ios::trunc);
+		cout << "Formateando texto " << k << "\n";
 
 		while (text >> word) {
 			transform(word.begin(), word.end(), word.begin(), ::tolower);
@@ -79,18 +83,45 @@ int main(int argc, char const *argv[])
 		text.close();
 
 		formatted.seekp(0, ios::beg);
-		h[i] = new Hashing(2 * filesize);
+		h[k] = new Hashing(filesize);
+		t[k] = new Ternario();
+		p[k] = new Ptrie();
 
-		cout << "Creando Hashing con " << (int) (2 * filesize) << " llaves posibles\n";
+		cout << "Creando Hashing con " << (int) (filesize) << " llaves posibles\n";
 		int pos = 0;
 		begin = chrono::high_resolution_clock::now();
 		while (formatted >> word) {
-			h[i].insert(word, pos);
+			h[k]->insert(word, pos);
 			pos += word.length() + 1;
 		}
 		end = chrono::high_resolution_clock::now();
 		elapsed = end - begin;
-		cout << h[i].unique_count() << " palabras distintas : " << elapsed.count() * 1000 << "ms\n";
+		cout << h[k]->unique_count() << " palabras distintas\n";
+		cout << "Hashing: " << elapsed.count() * 1000 << "ms\n";
+
+		pos = 0;
+		begin = chrono::high_resolution_clock::now();
+		while (formatted >> word) {
+			t[k]->insert(word, pos);
+			pos += word.length() + 1;
+		}
+		end = chrono::high_resolution_clock::now();
+		elapsed = end - begin;
+		cout << "Ternario: " << elapsed.count() * 1000 << "ms\n";
+
+		pos = 0;
+		begin = chrono::high_resolution_clock::now();
+		while (formatted >> word) {
+			p[k]->insert(word);
+			pos += word.length() + 1;
+		}
+		end = chrono::high_resolution_clock::now();
+		elapsed = end - begin;
+		cout << "Patricia: " << elapsed.count() * 1000 << "ms\n";
+
+		formatted.seekp(0, ios::beg);
+		formatted.close();
+		filesize = 0;
 	}
 
 	
